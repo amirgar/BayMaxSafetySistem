35e5af38c7fb2f06ac3f7d00b6e786e9937f3e8a
amirgar <garam08@bk.ru>
2024-04-30 19:45:40 +0300

    Добавление искусственного интеллекта для определения пользователя банкомата

--- ai/register_face.py
+++ ai/register_face.py
@@ -1,2 +1,44 @@
-from MTCNN_Detector import MTCNN_Detector
-from MTCNN_Detector import MTCNN
+import numpy as np
+import face_recognition
+import cv2
+import os
+
+
+def check_registration(path='photo/', check_name='photo') -> bool:
+    images = []
+    classNames = []
+    myList = os.listdir(path)
+    for cls in myList:
+        curImg = cv2.imread(f'{path}/{cls}')
+        images.append(curImg)
+        classNames.append(os.path.splitext(cls)[0])
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    encodeListKnown = encodeList
+    cap = cv2.VideoCapture(0)
+    while True:
+        success, img = cap.read()
+        imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+        imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+
+        facesCurFrame = face_recognition.face_locations(imgS)
+        encodeCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+
+        for encodeFace, faceLoc in zip(encodeCurFrame, facesCurFrame):
+            matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+            faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+            #print(faceDis)
+            matchIndex = np.argmin(faceDis)
+
+            if matches[matchIndex]:
+                name = classNames[matchIndex]
+                if check_name == name:
+                    return True
+
+
+if __name__ == "__main__":
+    if check_registration(check_name='photo'):
+        print("Вход в аккаунт произошел успешно")
--- main.py
+++ main.py
@@ -1,24 +1,16 @@
-from ai.get_photos import get_photos
-from ai.check_warnings import check_warnings
+from ai.get_photo import get_photo
+from ai.register_face import check_registration
 
-
-if __name__ == "__main__":
-    name = 'photo'
-    path = rf'C:\Users\Airat\PycharmProjects\BayMax_Bank\ai\photo'
-    counter = 3
-    get_photos(path=path, name=name, counter=counter, timer=0.5)
-    counter_angry = 0
-    counter_fear = 0
-    counter_old = 0
-    for i in range(1, counter + 1):
-        file = rf'{path}\photo{str(i)}.png'
-        print(file)
-        if check_warnings(img_path=file) == "Dangerous emoji: fear":
-            counter_fear += 1
-        if check_warnings(img_path=file) == "Dangerous emoji: angry":
-            counter_angry += 1
-        if check_warnings(img_path=file) == "Dangerous: old user":
-            counter_old += 1
-    print(f'Показатели страха: {counter_fear}')
-    print(f'Показатели злости: {counter_angry}')
-    print(f'Показатели старости: {counter_old}')
+type = input("Если вы хотите зарегистрироваться введите Sign Up, иначе Sign In: ")
+if type == 'Sign Up':
+    name = input('Введите ваше имя с оборотной стороны карты: ')
+    print('Встаньте прямо перед камерой, она будет снимать вас в ближайшие 10 секунд')
+    get_photo(path='ai/photo', name=name)
+    print('Регистрация прошла успешно, скоро будет доступен личный кабинет')
+else:
+    name = input('Введите ваше имя с оборотной стороны карты: ')
+    print('Встаньте прямо перед камерой, она будет снимать вас в ближайшие 10 секунд')
+    if check_registration(path='ai/photo', check_name=name):
+        print('Вход прошел успешно, скоро будет доступен личный кабинет')
+    else:
+        print('Не удалось войти в аккаунт. Повторите операцию снова')
