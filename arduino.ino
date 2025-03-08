f52589621562bdbf0e80f6bfbbf20422b16e9543
amirgar <81811152+amirgar@users.noreply.github.com>
2025-03-08 11:27:49 +0300

    Create arduino.ino

---
+++ arduino.ino
@@ -0,0 +1,12 @@
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(100000000);
+}
